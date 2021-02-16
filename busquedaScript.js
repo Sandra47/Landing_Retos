11eed722f7147859a7c03fd96e6f1b7f0ece7524
Sandra Lara <sandra.larafigueroa23@gmail.com>
2021-02-15 23:38:17 -0500

    Modificacion boton buscar

--- busquedaScript.js
+++ busquedaScript.js
@@ -1,34 +1,34 @@
 //mostrara la busqueda
-const input = document.querySelector('#input');
+const formulario = document.querySelector('#formulario');
 const boton = document.querySelector('#btn');
 const resultado=document.querySelector('#resultado');
 
 let galeria =[
-  {nombre: 'Expanding Cards', url: './Retos/Reto1/index.html', img:''},
-  {nombre: 'Progress Steps', url: './Retos/Reto2/index.html', img: ''},
-  {nombre: 'Rotating Navigation', url: './Retos/Reto3/index.html', img:''},
-//   {nombre: "Hidden Search", url: "../retos/Reto4/index.html", img: ""},
-//   {nombre: "Blurry Loading", url: "../retos/Reto5/index.html", img: ""},
-//   {nombre: "Drag N Drop", url: "../retos/Reto6/index.html", img: ""},
-//   {nombre: "AutoText", url: "../retos/Reto7/index.html", img: ""},
-//   {nombre: "Hoverboard", url: "../retos/Reto8/index.html", img: ""},
-//   {nombre: "Scroll Animation", url: "../retos/Reto9/index.html", img: ""},
-//   {nombre: "Form input wave", url: "../retos/Reto10/index.html", img: ""},
-//   {nombre: "Dad Jokes", url: "../retos/Reto11/index.html", img: ""},
-//   {nombre: "Sound Board", url: "../retos/Reto12/index.html", img: ""},
-//   {nombre: "Live User Filter", url: "../retos/Reto13/index.html", img: ""},
-//   {nombre: "Button Ripple Effect", url: "../retos/Reto14/index.html", img: ""},
-//   {nombre: "Background Slider", url: "../retos/Reto15/index.html", img: ""},
-//   {nombre: "Kinetic loader", url: "../retos/Reto16/index.html", img: ""},
-//   {nombre: "Netflix Navigation", url: "../retos/Reto17/index.html", img: ""},
-//   {nombre: "Testimonial Box Switcher", url: "../retos/Reto18/index.html", img: ""},
-//   {nombre: "Pokedex", url: "../retos/Reto19/index.html", img: ""},
-//   {nombre: "Doble click Heart", url: "../retos/Reto20/index.html", img: ""},
-//   {nombre: "Event Keycodes", url: "../retos/Reto21/index.html", img: ""},
-//   {nombre: "Mobile Nav Navigation", url: "../retos/Reto22/index.html", img: ""},
-//   {nombre: "Feedback UI Desing", url: "../retos/Reto23/index.html", img: ""},
-//   {nombre: "GitHub Profile", url: "../retos/Reto24/index.html", img: ""},
-//   {nombre: "Sticky Navigation", url: "../retos/Reto25/index.html", img: ""}
+  {nombre: 'Expanding Cards', url: './Retos/Reto1/index.html', img:'img/Reto1.png'},
+  {nombre: 'Progress Steps', url: './Retos/Reto2/index.html', img: 'img/Reto2.png'},
+  {nombre: 'Rotating Navigation', url: './Retos/Reto3/index.html', img:'img/Reto3.png'},
+  {nombre: "Hidden Search", url: "./Retos/Reto4/index.html", img: 'img/Reto4.png'},
+  {nombre: "Blurry Loading", url: "./Retos/Reto5/index.html", img: 'img/Reto5.png'},
+  {nombre: "Drag N Drop", url: "./Retos/Reto6/index.html", img: 'img/Reto6.png'},
+  {nombre: "AutoText", url: "./Retos/Reto7/index.html", img: 'img/Reto7.png'},
+  {nombre: "Hoverboard", url: "./Retos/Reto8/index.html", img: 'img/Reto8.png'},
+  {nombre: "Scroll Animation", url: "./Retos/Reto9/index.html", img: 'img/Reto9.png' },
+  {nombre: "Form input wave", url: "./Retos/Reto10/index.html", img:'img/Reto10.png'},
+  {nombre: "Dad Jokes", url: "./Retos/Reto11/index.html", img:'img/Reto11.png'},
+  {nombre: "Sound Board", url: "./Retos/Reto12/index.html", img:'img/Reto12.png'},
+  {nombre: "Live User Filter", url: "./Retos/Reto13/index.html", img: 'img/Reto13.png'},
+  {nombre: "Button Ripple Effect", url: "./Retos/Reto14/index.html", img:'img/Reto14.png'},
+  {nombre: "Background Slider", url: "./Retos/Reto15/index.html", img: 'img/Reto15.png'},
+  {nombre: "Kinetic loader", url: "./Retos/Reto16/index.html", img: 'img/Reto16.png'},
+  {nombre: "Netflix Navigation", url: "./Retos/Reto17/index.html", img:'img/Reto17.png'},
+  {nombre: "Testimonial Box Switcher", url: "./Retos/Reto18/index.html", img: 'img/Reto18.png'},
+  {nombre: "Pokedex", url: "./Retos/Reto19/index.html", img: 'img/Reto19.png'},
+  {nombre: "Doble click Heart", url: "./Retos/Reto20/index.html", img: 'img/Reto20.png'},
+  {nombre: "Event Keycodes", url: "./Retos/Reto21/index.html", img: 'img/Reto21.png'},
+  {nombre: "Mobile Nav Navigation", url: "./Retos/Reto22/index.html", img: 'img/Reto22.png'},
+  {nombre: "Feedback UI Desing", url: "./Retos/Reto23/index.html", img: 'img/Reto23.png'},
+  {nombre: "GitHub Profile", url: "./Retos/Reto24/index.html", img: 'img/Reto24.png'},
+  {nombre: "Sticky Navigation", url: "./Retos/Reto25/index.html", img: 'img/Reto25.png'}
 
 ]
 
@@ -39,28 +39,29 @@ const filtrar=()=>{
     
   resultado.innerHTML='';
 
- const texto = input.value.toLowerCase();
+ const texto = formulario.value.toLowerCase();
 
  for(let imagen of galeria){
    let nombre = imagen.nombre.toLowerCase();
    if(nombre.indexOf(texto) !== -1){//retona el elemento si existe o si no -1
      resultado.innerHTML +=`
-     <div class="galeria_item">
+     <div class="galeria_item col s12 m4 l3">
      <h2 class="titulo_Reto"><a href="${imagen.url}">${imagen.nombre}</a></h2>
       <img src="${imagen.img}" class= "galeria_img" data-caption="Progress Steps">
       </div>
       </div>
       `
+    // console.log("Funciona ",nombre)
     }
 
   }
   if (resultado.innerHTML===''){
-    resultado.innerHTML+=`<h2> no se ha encontrado el reto con el nombre:"${texto}"</h2>`
+    resultado.innerHTML+=`<h2> no se ha encontrado el reto con el nombre: "${texto}"</h2>`
 
   }
 
 }
 boton.addEventListener('click',filtrar)
-input.addEventListener('keyup', filtrar)
+formulario.addEventListener('keyup', filtrar)
  
 filtrar();
